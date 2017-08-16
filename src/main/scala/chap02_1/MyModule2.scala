130b16d8ab2787d6c2b86239b9d64b409fe1829d
arahansa <arahansa@naver.com>
2017-08-17 07:34:29 +0900

    add chapter 2

---
+++ src/main/scala/chap02_1/MyModule2.scala
@@ -0,0 +1,40 @@
+package chap02_1
+
+object MyModule2 {
+
+  def abs(n: Int): Int ={
+    if (n < 0) -n
+    else n
+  }
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial of %d is %d "
+    msg.format(n, factorial(n))
+  }
+
+
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
+  }
+
+  def factorial(n: Int): Int = {
+    def go(n: Int , acc: Int): Int =
+      if(n<=0) acc
+      else go(n-1, n*acc)
+    go(n,1)
+  }
+
+  def main(args: Array[String]): Unit =
+    println(formatAbs(-42))
+    println(formatFactorial(7))
+
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
+
+}
---
+++ src/main/scala/chap02_5/Practice.scala
@@ -0,0 +1,23 @@
+package chap02_5
+
+
+
+
+object Practice {
+
+  def isSorted[A](as:Array[A], ordered: (A,A) => Boolean ) : Boolean = {
+    def go(n: Int): Boolean ={
+      if(as.lengthCompare(n)==1) true
+      else if(ordered(as(n), as(n+1))) go(n + 1)
+      else false
+    }
+    go(0)
+  }
+
+  def main(args: Array[String]): Unit ={
+    def p[Integer](x:Int, y:Int ): Boolean = { x < y }
+    println("첫 번째 결과  :", isSorted(Array(1,2,3,4,5,6), p))
+    println("두 번째 결과  :", isSorted(Array(1,2,4,3,5,6), p))
+  }
+
+}
